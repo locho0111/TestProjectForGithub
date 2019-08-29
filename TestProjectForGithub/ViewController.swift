c908f110e56248001af4a487d567252df6b5351e
Loc Ho <loc.ho@smail.astate.edu>
2019-08-29 14:58:12 -0500

    testestest

---
+++ TestProjectForGithub/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  TestProjectForGithub
+//
+//  Created by Loc.Ho on 8/29/19.
+//  Copyright © 2019 Loc.Ho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+// I added a comment
+// asdjksdkjbadkjghaskjhajsdf
+    
+// asdkjhsdkjgdsfkjghdsfg
+}
+
